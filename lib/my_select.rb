5891d54c5f0c799ead87d0d9f3fa34fa7959fb1c
Matt Peleti <mattpeleti@gmail.com>
2016-09-25 19:50:02 +0000

    done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ cv= 0
+ newarray= []
+ while cv < collection.length
+   if yield(collection[cv])
+     newarray.push(collection[cv])
+   end
+   cv= cv + 1
+ end
+ newarray
+
 end
